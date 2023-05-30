3445f074def27af79130e7bf35d5e0da39a84dc2
AlipioGSF <alipiotec@gmail.com>
2023-05-29 23:27:08 -0300

    inicio back

---
+++ src/main/java/org/proj/residencia/ResidenciaApplication.java
@@ -0,0 +1,13 @@
+package org.proj.residencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/proj/residencia/ResidenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.proj.residencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
