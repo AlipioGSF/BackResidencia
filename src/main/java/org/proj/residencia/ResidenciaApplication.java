ae603876b6c0f83e6e3bfc44a1f125813b592dce
João Gabriel <joaogabriel2274@gmail.com>
2023-06-01 00:47:57 -0300

    Funcionando

--- src/main/java/org/proj/residencia/ResidenciaApplication.java
+++ src/main/java/org/proj/residencia/ResidenciaApplication.java
@@ -1,9 +1,11 @@
 package org.proj.residencia;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ResidenciaApplication {
 
 	public static void main(String[] args) {
